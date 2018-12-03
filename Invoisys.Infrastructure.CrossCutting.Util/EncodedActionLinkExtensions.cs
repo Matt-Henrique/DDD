70ba8e1336b67a6c5bc7e1a5b641eb70b6e25767
Mateus Henrique <mateus09h@gmail.com>
2018-12-03 01:44:24 -0200

    Removing Unused References

--- Invoisys.Infrastructure.CrossCutting.Util/EncodedActionLinkExtensions.cs
+++ Invoisys.Infrastructure.CrossCutting.Util/EncodedActionLinkExtensions.cs
@@ -1,48 +1,12 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace Invoisys.Infrastructure.CrossCutting.Util
 {
     public static class EncodedActionLinkExtensions
     {
-        public static MvcHtmlString EncodedActionLink(this HtmlHelper htmlHelper, string actionUrl, object routeValues,
-            object htmlAttributes, string iconClass)
-        {
-            var htmlAttributesString = string.Empty;
-            var queryString = string.Empty;
-            if (routeValues != null)
-            {
-                var d = new RouteValueDictionary(routeValues);
-                for (var i = 0; i < d.Keys.Count; i++)
-                {
-                    if (i > 0) queryString += "?";
-                    queryString += d.Keys.ElementAt(i) + "=" + d.Values.ElementAt(i);
-                }
-            }
-            if (htmlAttributes != null)
-            {
-                var d = new RouteValueDictionary(htmlAttributes);
-                for (var i = 0; i < d.Keys.Count; i++)
-                {
-                    htmlAttributesString += d.Keys.ElementAt(i) + "='" + d.Values.ElementAt(i) + "'";
-                }
-            }
-            var ancor = new StringBuilder();
-            ancor.Append("<a href='");
-            if (actionUrl != string.Empty) ancor.Append(actionUrl);
-            if (queryString != string.Empty) ancor.Append("?q=" + HttpUtility.UrlEncode(Encrypt(queryString)));
-            ancor.Append("' ");
-            if (htmlAttributesString != string.Empty) ancor.Append(htmlAttributesString);
-            ancor.Append("><i class='" + iconClass + "'></i>");
-            ancor.Append("");
-            return new MvcHtmlString(ancor.ToString());
-        }
         public static string Encrypt(string plainText)
         {
             const string key = "KeyInvoisys#";
@@ -53,11 +17,9 @@ namespace Invoisys.Infrastructure.CrossCutting.Util
                 var inputByte = Encoding.UTF8.GetBytes(plainText);
                 using (var ms = new MemoryStream())
                 {
-                    using (var cStream = new CryptoStream(ms, des.CreateEncryptor(encryptKey, iv), CryptoStreamMode.Write))
-                    {
-                        cStream.Write(inputByte, 0, inputByte.Length);
-                        cStream.FlushFinalBlock();
-                    }
+                    var cStream = new CryptoStream(ms, des.CreateEncryptor(encryptKey, iv), CryptoStreamMode.Write);
+                    cStream.Write(inputByte, 0, inputByte.Length);
+                    cStream.FlushFinalBlock();
                     return Convert.ToBase64String(ms.ToArray());
                 }
             }
@@ -72,11 +34,9 @@ namespace Invoisys.Infrastructure.CrossCutting.Util
                 var inputByte = Convert.FromBase64String(encryptedText);
                 using (var ms = new MemoryStream())
                 {
-                    using (var cs = new CryptoStream(ms, des.CreateDecryptor(decryptKey, iv), CryptoStreamMode.Write))
-                    {
-                        cs.Write(inputByte, 0, inputByte.Length);
-                        cs.FlushFinalBlock();
-                    }
+                    var cs = new CryptoStream(ms, des.CreateDecryptor(decryptKey, iv), CryptoStreamMode.Write);
+                    cs.Write(inputByte, 0, inputByte.Length);
+                    cs.FlushFinalBlock();
                     var encoding = Encoding.UTF8;
                     return encoding.GetString(ms.ToArray());
                 }
